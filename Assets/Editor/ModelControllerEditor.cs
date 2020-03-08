ef927150face5624755d005ef2b7167c5830e5d2
Xsoul1204 <nowqw123@gmail.com>
2020-03-08 17:52:51 +0800

    unity chan 模型控制 序列化字典

---
+++ Assets/Editor/ModelControllerEditor.cs
@@ -0,0 +1,75 @@
+﻿using act.game;
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(ModelController))]
+public class ModelControllerEditor : Editor
+{
+    private bool showActions = true;
+    private bool showTestChangeAction = true;
+    private bool showFaces = true;
+    private bool showTestChangeFace = true;
+    private bool keepAction = true;
+    private ModelController.Action actionChangeTo = ModelController.Action.IDLE;
+    private bool keepFace = true;
+    private ModelController.Face faceChangeTo = ModelController.Face.DEFAULT;
+    public override void OnInspectorGUI()
+    {
+        ModelController controller = (ModelController)target;
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour(target as MonoBehaviour), typeof(MonoScript), false);
+        EditorGUI.EndDisabledGroup();
+        controller.Animator = EditorGUILayout.ObjectField("Animator",controller.Animator, typeof(Animator),true) as Animator;
+        showActions = EditorGUILayout.Foldout(showActions, "Actions", true);
+        if (showActions)
+        {
+            var actionsList = new List<ModelController.Action>(controller.Actions.Keys);
+            foreach (var action in actionsList)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.Space(4);
+                EditorGUILayout.LabelField(action.ToString());
+                controller.Actions[action] = EditorGUILayout.ObjectField(controller.Actions[action], typeof(AnimationClip), true) as AnimationClip;
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+        showTestChangeAction = EditorGUILayout.Foldout(showTestChangeAction, "TestChangeAction", true);
+        var changeAction = false;
+        if (showTestChangeAction)
+        {
+            actionChangeTo = (ModelController.Action)EditorGUILayout.EnumPopup("Action", actionChangeTo);
+            keepAction = EditorGUILayout.Toggle("KeepAction", keepAction);
+            changeAction = GUILayout.Button("Change Action");
+        }
+        if (changeAction)
+        {
+            controller.ChangeAction(actionChangeTo, keepAction);
+        }
+        showFaces = EditorGUILayout.Foldout(showFaces, "Faces", true);
+        if (showFaces)
+        {
+            var facesList = new List<ModelController.Face>(controller.Faces.Keys);
+            foreach (var face in facesList)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.Space(4);
+                EditorGUILayout.LabelField(face.ToString());
+                controller.Faces[face] = EditorGUILayout.ObjectField(controller.Faces[face], typeof(AnimationClip), true) as AnimationClip;
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+        showTestChangeFace = EditorGUILayout.Foldout(showTestChangeFace, "TestChangeFace", true);
+        var changeFace = false;
+        if (showTestChangeFace)
+        {
+            faceChangeTo = (ModelController.Face)EditorGUILayout.EnumPopup("Face", faceChangeTo);
+            keepFace = EditorGUILayout.Toggle("KeepFace", keepFace);
+            changeFace = GUILayout.Button("Change Face");
+        }
+        if (changeFace)
+        {
+            controller.ChangeFace(faceChangeTo, keepFace);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/ModelController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+namespace act
+{
+    namespace game
+    {
+        public class ModelController : SingletonMonoBehaviorNoDestroy<GameController>
+        {
+            public enum Action
+            {
+                IDLE,
+                JUMP,
+                WALK
+            };
+            public enum Face
+            {
+                DEFAULT,
+                ANGRY,
+                SMILE,
+                ASHAMED,
+                SURPRISE,
+                EYE_CLOSE,
+                MOUTH_A,
+                MOUTH_E,
+                MOUTH_I,
+                MOUTH_O,
+                MOUTH_U
+            };
+            public Animator Animator = null;
+            [Serializable]
+            public class ActionAnimationClipDictionary : SerializableDictionary<Action, AnimationClip> { }
+            public ActionAnimationClipDictionary Actions = new ActionAnimationClipDictionary
+            {
+                {Action.IDLE ,null},
+                {Action.JUMP ,null},
+                {Action.WALK ,null}
+            };
+            [Serializable]
+            public class FaceAnimationClipDictionary : SerializableDictionary<Face, AnimationClip> { }
+            public FaceAnimationClipDictionary Faces = new FaceAnimationClipDictionary
+            {
+                {Face.DEFAULT,null},
+                {Face.ANGRY,null},
+                {Face.SMILE,null},
+                {Face.ASHAMED,null},
+                {Face.SURPRISE,null},
+                {Face.EYE_CLOSE,null},
+                {Face.MOUTH_A,null},
+                {Face.MOUTH_E,null},
+                {Face.MOUTH_I,null},
+                {Face.MOUTH_O,null},
+                {Face.MOUTH_U,null},
+            };
+            private static int FACE_LAYER = 1;
+            private bool keepAction = true;
+            private bool keepFace = true;
+            private void Update()
+            {
+                if (Animator == null)
+                    return;
+                if (Actions[Action.IDLE] != null && !keepAction)
+                {
+                    var actionStateInfo = Animator.GetCurrentAnimatorStateInfo(0);
+                    if (!actionStateInfo.IsName(Actions[Action.IDLE].name) && actionStateInfo.normalizedTime >= 1.0f)
+                        Animator.CrossFade(Actions[Action.IDLE].name, 0.0f);
+                }
+                if (Faces[Face.DEFAULT] != null && !keepFace)
+                {
+                    var faceStateInfo = Animator.GetCurrentAnimatorStateInfo(FACE_LAYER);
+                    if (!faceStateInfo.IsName(Faces[Face.DEFAULT].name) && faceStateInfo.normalizedTime >= 1.0f)
+                        Animator.CrossFade(Faces[Face.DEFAULT].name, 0.0f, FACE_LAYER);
+                }
+            }
+
+            public void ChangeAction(Action action, bool keep = true)
+            {
+                if (Animator == null)
+                    return;
+                if (!Enum.IsDefined(typeof(Action), action))
+                    return;
+                if (Actions[action] == null)
+                    return;
+                keepAction = keep;
+                Animator.CrossFade(Actions[action].name, 0.0f);
+            }
+
+            public void ChangeFace(Face face, bool keep = true)
+            {
+                if (Animator == null)
+                    return;
+                if (!Enum.IsDefined(typeof(Face), face))
+                    return;
+                if (Faces[face] == null)
+                    return;
+                keepFace = keep;
+                Animator.SetLayerWeight(FACE_LAYER, 1.0f);
+                Animator.CrossFade(Faces[face].name, 0.0f, FACE_LAYER);
+            }
+        }
+    }
+}
